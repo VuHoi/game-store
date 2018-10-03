ee299f02cbdb52af3a0588f4793c9af3e1c28599
hoi <toilahoi007@gmail.com>
2018-10-04 00:13:57 +0700

    add demo seed data

--- GameStore/Data/ApplicationDBContext.cs
+++ GameStore/Data/ApplicationDBContext.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using GameStore.Model;
 namespace GameStore.Data
 {
@@ -22,19 +21,34 @@ namespace GameStore.Data
         {
         }
 
-     
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
+
+            base.OnModelCreating(builder);
             builder.Entity<UserGame>()
            .HasKey(t => new { t.UserId, t.GameId });
-            base.OnModelCreating(builder);
-           
+            builder.Entity<Publisher>()
+           .HasMany(p => p.Games)
+           .WithOne(p => p.Publisher);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
 
+            //seed data 
+            var deviceCategorys = new[]
+            {
+                new CodeFree(){ Id = Guid.NewGuid(), Code = "Monitor"},
+                new CodeFree(){ Id = Guid.NewGuid(), Code = "Keyboard"},
+                new CodeFree(){ Id = Guid.NewGuid(), Code = "Computer"},
+                new CodeFree(){ Id = Guid.NewGuid(), Code = "VGA cable"},
+                new CodeFree(){ Id = Guid.NewGuid(), Code = "Power cable"},
+            };
+
+            builder.Entity<CodeFree>().HasData(deviceCategorys);
 
         }
 
-        }
+    }
 }
---
+++ GameStore/Migrations/20180927160126_addpublisher-One-To_many.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GameStore.Migrations
+{
+    public partial class addpublisherOneTo_many : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
