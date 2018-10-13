da3737e907b3573627470020ddd814a727a64f91
hoi <toilahoi007@gmail.com>
2018-10-13 15:30:12 +0700

    add annotations

---
+++ GameStore/DataAnnotations/FutureDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameStore.DataAnnotations
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return Convert.ToDateTime(value) > DateTime.Now;
+        }
+    }
+}
